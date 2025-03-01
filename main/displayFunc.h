b0bb32ff070f246b895454b95fcb652f271ce3bb
iCodeAlot1001 <gamotka23@gmail.com>
2025-03-01 20:50:54 +0800

    test2

---
+++ main/displayFunc.h
@@ -0,0 +1,9 @@
+#ifndef DISPLAYFUNC_H
+#define DISPLAYFUNC_H
+
+void btn1();
+void btn2();
+void btn3();
+void blank();
+
+#endif
---
+++ main/wifi_scan.h
@@ -0,0 +1,5 @@
+#ifndef WIFI_SCAN_H
+#define WIFI_SCAN_H
+void wifi_scan();
+void wifi_scan_repeat();
+#endif
